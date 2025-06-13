7dec3c9778ce9822693335d98c81ae942c60e24b
Vekaltor <56607344+Vekaltor@users.noreply.github.com>
2025-06-13 20:13:14 +0200

    ADD friendships actions

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,26 +1,49 @@
+// import { connect, set } from "mongoose";
+// import { createViewMessagesPerConversation } from "./dbViews";
+//
+// //MONGOOSE
+// const DB = {
+//   options: {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     auth: {
+//       authSource: process.env.DB_AUTHSOURCE,
+//     },
+//     user: process.env.DB_ROOT,
+//     pass: process.env.DB_PASS,
+//   },
+// };
+//
+// set("strictQuery", false);
+// const initConnectDB = () => {
+//   console.log("init");
+//   connect(process.env.DB_CONNECT, DB.options, (err, db) => {
+//     !err
+//       ? console.log("MongoDB connected")
+//       : console.log("MongoDB not connected ", err);
+//
+//     createViewMessagesPerConversation();
+//   });
+// };
+//
+// export default initConnectDB;
+
+
 import { connect, set } from "mongoose";
 import { createViewMessagesPerConversation } from "./dbViews";
 
-//MONGOOSE
-const DB = {
-  options: {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    auth: {
-      authSource: process.env.DB_AUTHSOURCE,
-    },
-    user: process.env.DB_ROOT,
-    pass: process.env.DB_PASS,
-  },
-};
-
 set("strictQuery", false);
+
 const initConnectDB = () => {
   console.log("init");
-  connect(process.env.DB_CONNECT, DB.options, (err, db) => {
+
+  connect(process.env.DB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }, (err, db) => {
     !err
-      ? console.log("MongoDB connected")
-      : console.log("MongoDB not connected ", err);
+        ? console.log("MongoDB connected")
+        : console.log("MongoDB not connected ", err);
 
     createViewMessagesPerConversation();
   });
